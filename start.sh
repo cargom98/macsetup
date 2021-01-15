c32937b3b1c489b8efae1d2fb0890809558fc606
Carlos Gomez <cgomez@carlosgomez.net>
2021-01-15 15:21:31 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
+brew install slack
+brew install zoom
+brew install google-chrome
+brew install microsoft-office
